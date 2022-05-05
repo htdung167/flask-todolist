e8ecda68be835fbedebc331f0c91e07feedad290
Dung Hoang Tien <htdung167@gmail.com>
2022-05-05 22:17:08 +0700

    build models

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-from todolist.user import user
-from todolist.views import views
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 load_dotenv()
@@ -10,9 +11,24 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DB_NAME = os.environ.get("DB_NAME")
 
 
+def create_database(app):
+    if not os.path.exists('todolist/' + DB_NAME):
+        db.create_all(app=app)
+        print("Created DB!")
+
+
 def create_app():
     app = Flask(__name__)
+    from todolist.user import user
+    from todolist.views import views
+    from todolist.models import User, Note
+
     app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
+    create_database(app)
+
     app.register_blueprint(user)
     app.register_blueprint(views)
     return app
