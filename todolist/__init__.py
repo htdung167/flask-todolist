ef23b57c37c130d346c3be215652097e8b7846fb
Dung Hoang Tien <htdung167@gmail.com>
2022-05-05 16:45:37 +0700

    build templates

--- todolist/__init__.py
+++ todolist/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+from todolist.user import user
+from todolist.views import views
+
+
+load_dotenv()
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
 
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.register_blueprint(user)
+    app.register_blueprint(views)
     return app
---
+++ todolist/user.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+user = Blueprint("user", __name__)
+
+
+@user.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@user.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@user.route("/logout")
+def logout():
+    return "Logout Page"
